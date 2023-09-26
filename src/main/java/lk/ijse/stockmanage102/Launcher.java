ff03e01717659105dd6401c696fbb88e109a58ca
Harsha-Pradeep377 <harshapradeep9@gmail.com>
2023-09-27 02:01:13 +0530

    Dash board form completed

---
+++ src/main/java/lk/ijse/stockmanage102/Launcher.java
@@ -0,0 +1,23 @@
+package lk.ijse.stockmanage102;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent rootnNode = FXMLLoader.load(this.getClass().getResource("/view/stock_manage.fxml"));
+        Scene scene = new Scene(rootnNode);
+        stage.setScene(scene);
+
+        stage.setTitle("Stock Manage Form");
+        stage.centerOnScreen();
+        stage.show();
+    }
+}
---
+++ src/main/java/lk/ijse/stockmanage102/LauncherWrapper.java
@@ -0,0 +1,10 @@
+package lk.ijse.stockmanage102;
+
+import lk.ijse.stockmanage102.Launcher;
+
+public class LauncherWrapper {
+    public static void main(String[] args) {
+        Launcher.main(args);
+    }
+
+}
