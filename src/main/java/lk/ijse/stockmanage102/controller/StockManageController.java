7c9c577fd5648f6b637a6f8da6a37a5af9d4f00e
Harsha-Pradeep377 <harshapradeep9@gmail.com>
2023-09-27 11:54:35 +0530

    navigation to item form completed

--- src/main/java/lk/ijse/stockmanage102/controller/StockManageController.java
+++ src/main/java/lk/ijse/stockmanage102/controller/StockManageController.java
@@ -4,11 +4,16 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class StockManageController {
+
+
+    public AnchorPane rootNode;
+
     public void btnCustomerOnAction(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(this.getClass().getResource("/view/customer_form.fxml"));
         Scene scene = new Scene(root);
@@ -17,5 +22,16 @@ public class StockManageController {
         stage.setTitle("Customer Manage Form");
         stage.centerOnScreen();
         stage.show();
+    }
+
+    public void btnItemOnAction(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(this.getClass().getResource("/view/item_form.fxml"));
+        Scene scene = new Scene(root);
+
+        Stage primaryStage = (Stage) this.rootNode.getScene().getWindow();
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Item Manage Form");
+        primaryStage.centerOnScreen();
+
     }
 }
