f00ee91c054cec444eae85ef600bd3b8e3ad6936
Harsha-Pradeep377 <harshapradeep9@gmail.com>
2023-09-28 23:16:37 +0530

    back button action completed

--- src/main/java/lk/ijse/stockmanage102/controller/ItemFormController.java
+++ src/main/java/lk/ijse/stockmanage102/controller/ItemFormController.java
@@ -1,4 +1,24 @@
 package lk.ijse.stockmanage102.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ItemFormController {
+    public AnchorPane itemNode;
+
+    public void btnDachboardOnAction(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(this.getClass().getResource("/view/stock_manage.fxml"));
+        Scene scene = new Scene(root);
+
+        Stage stage = (Stage) this.itemNode.getScene().getWindow();
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.setTitle("Stock Manage Form");
+    }
 }
