33d7bf3dd2ff671652482c4c140ed74fe4afe815
Harsha-Pradeep377 <harshapradeep9@gmail.com>
2023-10-04 00:50:55 +0530

    navigate from login form

--- src/main/java/lk/ijse/stockmanage102/controller/CustomerFormController.java
+++ src/main/java/lk/ijse/stockmanage102/controller/CustomerFormController.java
@@ -1,5 +1,49 @@
 package lk.ijse.stockmanage102.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import lk.ijse.stockmanage102.db.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class CustomerFormController {
 
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtTel;
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+        String id = txtId.getText();
+        String name = txtName.getText();
+        String address = txtAddress.getText();
+        String tel = txtTel.getText();
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            String sql = "INSERT INTO customer VALUES(?,?,?,?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,id);
+            preparedStatement.setString(2,name);
+            preparedStatement.setString(3,address);
+            preparedStatement.setString(4,tel);
+
+            boolean isSaved = preparedStatement.executeUpdate()>0;
+            if (isSaved){
+                new Alert(Alert.AlertType.CONFIRMATION, "Customer saved!").show();
+            }
+
+
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
+    }
+    public void btnClearOnAction(ActionEvent actionEvent) {
+        txtId.setText("");
+        txtName.setText("");
+        txtAddress.setText("");
+        txtTel.setText("");
+    }
 }
